266e5cbbdb0e37d9c1805ed1b22941c915fb8bbc
Nikita Gerasimov <tariel-x@ya.ru>
2015-03-14 20:57:28 +0300

    Initia;

---
+++ src/test/java/org/tariel/pla/AppTest.java
@@ -0,0 +1,19 @@
+package org.tariel.pla;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
