cc341990614af4a77140623e027853853bc9d4f4
Nikita Gerasimov <tariel-x@ya.ru>
2015-04-02 14:23:42 +0300

    Ready without prepositions

--- src/main/java/org/tariel/pla/sentence/ConllWord.java
+++ src/main/java/org/tariel/pla/sentence/ConllWord.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.lang.StringUtils;
+import org.tariel.pla.logics.ConjunctionContainer;
 import org.tariel.pla.logics.IFunction;
 import org.tariel.pla.logics.LogicFunction;
 import org.tariel.pla.logics.Quantifer;
@@ -31,9 +32,10 @@ import org.tariel.pla.logics.Term;
  */
 public class ConllWord implements IWord
 {
+
     public List<IWord> subwords;
     public Link linktype;
-    
+
     public String word;
     public String lex;
     public String pos;
@@ -53,21 +55,21 @@ public class ConllWord implements IWord
     public String transitivity;
     public String addditionalinfo;
     public String other;
-    
+
     public Integer id;
-    
+
     @Override
     public List<IWord> getSubWords()
     {
-        return this.subwords;
+	return this.subwords;
     }
 
     @Override
     public Link getLink()
     {
-        return this.linktype;
+	return this.linktype;
     }
-    
+
     @Override
     public String getWord()
     {
@@ -169,7 +171,7 @@ public class ConllWord implements IWord
     {
 	return this.addditionalinfo;
     }
-    
+
     @Override
     public Link getLinktype()
     {
@@ -271,13 +273,13 @@ public class ConllWord implements IWord
     {
 	this.addditionalinfo = value;
     }
-    
+
     @Override
     public void setWord(String value)
     {
 	this.word = value;
     }
-    
+
     @Override
     public void setLinktype(Link link)
     {
@@ -290,7 +292,7 @@ public class ConllWord implements IWord
 	List<String> conllstrings = Arrays.asList(conllSentence.split("\n"));
 	//find root
 	int root = 0;
-	for (int i=0; i<conllstrings.size(); i++)
+	for (int i = 0; i < conllstrings.size(); i++)
 	{
 	    String[] parts = conllstrings.get(i).split("\t");
 	    if (Integer.parseInt(parts[6]) == 0)
@@ -301,20 +303,23 @@ public class ConllWord implements IWord
 		this.lex = parts[2];
 		this.linktype = ConllWord.decodeLink(parts[7]);
 		if (this.Pos.contains(parts[3]))
+		{
 		    this.pos = parts[3];
-		else
+		} else
+		{
 		    this.pos = "OTHER";
+		}
 		this.decodeParams(parts[5]);
 		root = i;
 		break;
 	    }
 	}
-	this.fromConll(conllSentence, root+1);
+	this.fromConll(conllSentence, root + 1);
     }
-  
+
     public void decodeParams(String params)
     {
-	List<String> parts = Arrays.asList( StringUtils.split(params, "|"));
+	List<String> parts = Arrays.asList(StringUtils.split(params, "|"));
 	for (Field field : IWord.class.getDeclaredFields())
 	{
 	    if (field.getName().contains("Variants"))
@@ -322,7 +327,7 @@ public class ConllWord implements IWord
 		try
 		{
 		    String name = field.getName().replace("Variants", "");
-		    List<String> values = Arrays.asList((String[])field.get(this));
+		    List<String> values = Arrays.asList((String[]) field.get(this));
 		    for (String part : parts)
 		    {
 			if (values.contains(part))
@@ -331,7 +336,7 @@ public class ConllWord implements IWord
 			    Field thisField = ConllWord.class.getField(fieldName);
 			    thisField.set(this, part);
 			}
-		    } 
+		    }
 		} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException ex)
 		{
 		    ex.printStackTrace();
@@ -339,7 +344,7 @@ public class ConllWord implements IWord
 	    }
 	}
     }
-    
+
     public static Link decodeLink(String link)
     {
 	switch (link)
@@ -362,14 +367,14 @@ public class ConllWord implements IWord
 		return Link.NONE;
 	}
     }
-    
+
     public void fromConll(String conllSentence, int me)
     {
 	List<String> conllstrings = Arrays.asList(conllSentence.split("\n"));
 	//find every child word
 	int root = 0;
 	this.subwords = new ArrayList<>();
-	for (int i=0; i<conllstrings.size(); i++)
+	for (int i = 0; i < conllstrings.size(); i++)
 	{
 	    String[] parts = conllstrings.get(i).split("\t");
 	    if (Integer.parseInt(parts[6]) == me)
@@ -381,11 +386,14 @@ public class ConllWord implements IWord
 		tmp.setLex(parts[2]);
 		tmp.setLinktype(ConllWord.decodeLink(parts[7]));
 		if (this.Pos.contains(parts[3]))
+		{
 		    tmp.setPos(parts[3]);
-		else
+		} else
+		{
 		    tmp.setPos("OTHER");
+		}
 		tmp.decodeParams(parts[5]);
-		tmp.fromConll(conllSentence, i+1);
+		tmp.fromConll(conllSentence, i + 1);
 		this.subwords.add(tmp);
 	    }
 	}
@@ -399,29 +407,32 @@ public class ConllWord implements IWord
 	for (IWord subword : this.subwords)
 	{
 	    if (subword.getLink() == linktype)
+	    {
 		retwords.add(subword);
+	    }
 	}
 	return retwords;
     }
-        
+
     @Override
     public IFunction toPLA()
     {
+	//TODO: запилить местоимения - самое главное же
 	if (this.pos.equals("V"))
 	{
 	    //Predicate term (verb)
 	    //P(?)
 	    Term predic_predic = new Term();
 	    predic_predic.setName(this.getLex());
-	    
+
 	    //variables for ∃xSx
 	    Quantifer quant_subj = null;//Base quantifer - root element for this part
 	    Term subj_term = null;
-	    
+
 	    //variables for ∃xOx
 	    Quantifer quant_obj = null;
 	    Term obj_predic = null;
-	    
+
 	    //Find verb's subject
 	    //construnct ∃xSx
 	    IWord subj_word = this.getSubject();
@@ -434,13 +445,12 @@ public class ConllWord implements IWord
 		subj_term = new Term();
 		subj_term.addVar(quant_subj.getVar());
 		subj_term.setName(subj_word.getLex());
-	    }
-	    else
+	    } else
 	    {
 		//nothing will be happened here
 		return null;
 	    }
-	    
+
 	    //Search for verb's objects
 	    //constructs ∃yOy
 	    IWord obj_word = this.getObject();
@@ -454,13 +464,17 @@ public class ConllWord implements IWord
 		obj_predic.addVar(quant_obj.getVar());
 		obj_predic.setName(obj_word.getLex());
 	    }
-	    
+
 	    //Add subject and object variables to predicate (if they exist)
 	    //P(?) -> Pxy or Px or Py or nothing
 	    if (quant_subj != null)
+	    {
 		predic_predic.addVar(quant_subj.getVar());
+	    }
 	    if (quant_obj != null)
+	    {
 		predic_predic.addVar(quant_obj.getVar());
+	    }
 
 	    //Object quantifer for Object and Predicate
 	    //∃y(Oy ⋀ Pxy) or nothing
@@ -469,73 +483,115 @@ public class ConllWord implements IWord
 		quant_obj.addSub(obj_predic);
 		quant_obj.addSub(predic_predic);
 	    }
-	    
+
 	    //Subject first for previous quantifer and Subject term
 	    //∃x(Sx ⋀ ∃y(Oy ⋀ Pxy)) or ∃x(Sx ⋀ Px)
 	    if (quant_obj != null)
 	    {
 		quant_subj.addSub(subj_term);
 		quant_subj.addSub(quant_obj);
-	    }
-	    else
+	    } else
 	    {
 		quant_subj.addSub(subj_term);
 		quant_subj.addSub(predic_predic);
 	    }
 
-	    IFunction total = null;
+	    //If will be found something deeped - this will be for conjunction of current and deeper
+	    IFunction total = new ConjunctionContainer();
+	    //add current result
+	    total.addSub(quant_subj);
+	    //result of object's subcalculations
+	    IFunction obj_underlogic = null;
 	    IFunction subj_underlogic = subj_word.toPLA();
 	    if (!obj_word.isBlank())
 	    {
-		IFunction sobj_underlogic = obj_word.toPLA();
+		obj_underlogic = obj_word.toPLA();
 	    }
-	    
-	    
-	}
-	else if (this.pos.equals("S"))
+	    if (subj_underlogic != null)
+	    {
+		total.addSub(subj_underlogic);
+	    }
+	    if (obj_underlogic != null)
+	    {
+		total.addSub(obj_underlogic);
+	    }
+	    //if total contains only current result and no sublogic - return only current
+	    if (total.getSub().size() > 1)
+	    {
+		return total;
+	    } else
+	    {
+		return quant_subj;
+	    }
+	} else if (this.pos.equals("S"))
 	{
+	    //construnct ∃xSx
 	    Quantifer quant = new Quantifer();
 	    Term word = new Term();
 	    word.setName(this.lex);
 	    quant.addSub(word);
-	    IFunction total = null;
+
+	    IFunction total = new ConjunctionContainer();
+	    total.addSub(quant);
 	    for (IWord subword : this.subwords)
 	    {
-		ConllWord conll_sub = (ConllWord)subword;
-		total = conll_sub.toPLA(quant);
+		ConllWord conll_sub = (ConllWord) subword;
+		total.addSub(conll_sub.toPLA());
 	    }
-	    return total;
-	}
-	else
+	    //if total contains only current result and no sublogic - return only current
+	    if (total.getSub().size() > 1)
+	    {
+		return total;
+	    } else
+	    {
+		return quant;
+	    }
+	} else
 	{
+	    IFunction total = new ConjunctionContainer();
 	    for (IWord subword : this.subwords)
 	    {
-		subword.toPLA();
+		ConllWord conll_sub = (ConllWord) subword;
+		total.addSub(conll_sub.toPLA());
+	    }
+	    //if total contains one result and no sublogic - return only one
+	    if (total.getSub().size() > 1)
+	    {
+		return total;
+	    } else if (total.getSub().size() > 0)
+	    {
+		return total.getSub().get(0);
+	    } else
+	    {
+		return null;
 	    }
 	}
-	return null;
     }
-    
+
     /**
      * Finds logic constructions and returns input formula AND subformula
+     *
      * @param current
-     * @return 
+     * @return
      */
     public IFunction toPLA(IFunction current)
     {
+	IFunction total = new ConjunctionContainer();
 	IFunction new_funcs = this.toPLA();
 	if (new_funcs != null)
 	{
-	    LogicFunction conjuct = new LogicFunction();
-	    conjuct.addSub(current);
-	    conjuct.addSub(new_funcs);
-	    return conjuct;
+	    total.addSub(current);
+	    total.addSub(new_funcs);
+	    return total;
 	}
 	return current;
     }
 
     @Override
-    public IWord getSubject() {
+    public IWord getSubject()
+    {
+	//TODO: искать не только по подсловам, но и глубже
+	//Возможно, это стоит вынести в getSubWords
 	List<IWord> objects = this.getSubWords(IWord.Link.SUBJ);
 	for (IWord obj : objects)
 	{
@@ -551,7 +607,9 @@ public class ConllWord implements IWord
     }
 
     @Override
-    public IWord getObject() {
+    public IWord getObject()
+    {
+	//TODO: искать не только по подсловам, но и глубже
 	List<IWord> objects = this.getSubWords(IWord.Link.OBJ);
 	for (IWord obj : objects)
 	{
@@ -567,7 +625,8 @@ public class ConllWord implements IWord
     }
 
     @Override
-    public Boolean isBlank() {
+    public Boolean isBlank()
+    {
 	return (this.linktype == IWord.Link.NONE);
     }
 }
